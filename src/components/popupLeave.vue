74d8fe90713a0438811206e3704f1fc2222a4192
inyod <inyod2540ekkachai@gmail.com>
2020-01-07 18:10:06 +0700

    Edit popup Leave,OT

--- src/components/popupLeave.vue
+++ src/components/popupLeave.vue
@@ -13,81 +13,98 @@
         </div>
       </b-col>
     </div>
-    <vs-popup  classContent="popup-example"  title="ประวัติการลาของนายเอกชัย หมโมลี" :active.sync="popupLeave" >
+    <vs-popup id="sizePopupLeave" classContent="popupLeave-example"  title="ประวัติการลาของนายเอกชัย หมโมลี" :active.sync="popupLeave" >
       <center><h4>นายเอกชัย หมโมลี</h4></center>
       <center><h5>แผนก : โปรแกรมเมอร์</h5></center>
         <div>
-              <div style="margin-top:25px">
-                <b-row class="my-1" v-for="type in types" :key="type">
-                  <b-col>
-                    <div class="form-group" :class="{ 'form-group--error': $v.form.valDate1.$error }">
-                      <p>ขอลาในวันที่ :</p>
-                      <datetime v-model.trim="$v.form.valDate1.$model" format="DD/MM/YYYY H:i" style="width:255px;height:37px;cursor: pointer;"></datetime>
-                      <div class="error" v-if="!$v.form.valDate1.required"><font color="red">Require</font></div>
-                      <div class="error" v-else><font color="Success">Success</font></div>
-                    </div>
-                  </b-col>
-                  <b-col>
-                    <div class="form-group" :class="{ 'form-group--error': $v.form.valDate2.$error }">
-                      <p>ถึงวันที่(กรณีลามากกว่า 1 วัน) :</p>
-                      <datetime v-model.trim="$v.form.valDate2.$model" style="width:255px;height:37px;cursor: pointer;" format="DD/MM/YYYY H:i"></datetime>
-                      <div class="error" v-if="!$v.form.valDate2.required"><font color="red">Require</font></div>
-                      <div class="error" v-else><font color="Success">Success</font></div>
-                    </div>
-                  </b-col>
-                </b-row>
-              </div>   
-              <div class="con-select-example">
-                <b-row>
-                  <b-col>
-                    <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.leaveReason.$error }"> -->
-                      <p style="margin-bottom:-10px">เหตุผลการลา :</p>
-                        <b-form-select 
-                          label="เหตุผลการลา"
-                          v-model="selected1" 
-                          :options="options1" 
-                          size="sm" 
-                          class="mt-3" 
-                          style="width:235px;height:37px; margin-bottom:8px; cursor: pointer;"
-                          
-                        >
-                        </b-form-select> 
-                        <!-- <div class="error" v-if="!$v.form.leaveReason.required">Require</div>
-                        <div class="error" v-else>Success</div>
-                    </div>  -->
-                  </b-col>
-                  <b-col>
-                    <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.leaveType.$error }"> -->
-                      <p>ประเภทการลา</p>
-                        <b-form-radio-group
-                          v-model="selected"
-                          :options="options"
-                          
-                        ></b-form-radio-group>    
-                        <!-- <div class="error" v-if="!$v.form.leaveType.required">Require</div>
-                        <div class="error" v-else>Success</div>
-                    </div> -->
-                  </b-col>            
-                </b-row>
-              </div>
-              <div>
-                <div class="form-group" :class="{ 'form-group--error': $v.form.description.$error }">
-                  <p>รายะเอียดการลา</p>
-                  <vs-textarea style="width:340px;height:80px; padding:1px" v-model.trim="$v.form.description.$model"></vs-textarea>
-                  <div class="error" v-if="!$v.form.description.required"><font color="red">Require</font></div>
-                  <div class="error" v-else><font color="Success">Success</font></div>
-                </div> 
-              </div>    
-            </div>
-              <div style="margin-top:25px">
-                <b-col sm="12">
-                  <center>
-                    <vs-button style="margin-top:0px" @click="popupActivo3=true" color="primary" type="filled" to="/leaveUser">
-                      บันทึก
-                    </vs-button>
-                  </center>
-                </b-col>
-              </div>
+          <div>
+            <b-row class="my-1" v-for="type in types" :key="type">
+              <b-col>
+                <div class="form-group" :class="{ 'form-group--error': $v.form.valDate1.$error }">
+                  <p>ขอลาในวันที่ :</p>
+                  <datetime v-model.trim="$v.form.valDate1.$model" format="DD/MM/YYYY H:i" style="width:250px;height:37px;cursor: pointer;"></datetime>
+                  <div class="error" v-if="!$v.form.valDate1.required"><font color="red">*จำเป็น</font></div>
+                  <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+                </div>
+              </b-col>
+              <b-col>
+                <div style="margin-bottom:2px" class="form-group" :class="{ 'form-group--error': $v.form.valDate2.$error }">
+                  <p>ถึงวันที่(กรณีลามากกว่า 1 วัน) :</p>
+                  <datetime v-model.trim="$v.form.valDate2.$model" style="width:250px;height:37px;cursor: pointer;" format="DD/MM/YYYY H:i"></datetime>
+                  <div class="error" v-if="!$v.form.valDate2.required"><font color="red">*จำเป็น</font></div>
+                  <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+                </div>
+              </b-col>
+            </b-row>
+          </div>
+          <div class="con-select-example">
+            <b-row>
+              <b-col>
+                <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.leaveReason.$error }"> -->
+                  <p style="margin-bottom:-10px">เหตุผลการลา :</p>
+                    <b-form-select 
+                      label="เหตุผลการลา"
+                      v-model="selected1" 
+                      :options="options1" 
+                      size="sm" 
+                      class="mt-3" 
+                      style="width:235px;height:37px; margin-bottom:8px; cursor: pointer;"
+                    >
+                    </b-form-select> 
+                    <!-- <div class="error" v-if="!$v.form.leaveReason.required">Require</div>
+                    <div class="error" v-else>Success</div>
+                </div>  -->
+              </b-col>
+              <b-col>
+                <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.leaveType.$error }"> -->
+                  <p>ประเภทการลา</p>
+                    <b-form-radio-group
+                      v-model="selected"
+                      :options="options"
+                    >
+                    </b-form-radio-group>    
+                    <!-- <div class="error" v-if="!$v.form.leaveType.required">Require</div>
+                    <div class="error" v-else>Success</div>
+                </div> -->
+              </b-col>            
+            </b-row>
+          </div>
+          <div>
+            <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.descriptionWork.$error }">
+              <p>รายะเอียดการลา :</p>
+                <b-form-textarea
+                  v-model.trim="$v.form.descriptionWork.$model"
+                  style="width:340px; padding:1px"
+                  placeholder="กรอกรายละเอียดการลา"
+                >
+                </b-form-textarea>
+                <div class="error" v-if="!$v.form.descriptionWork.required"><font color="red">*จำเป็น</font></div>
+                <div class="error" v-else><font color="Success"></font></div>
+            </div> -->
+          <div style="margin-top:10px" class="form-group" :class="{ 'form-group--error': $v.form.description.$error }">
+            <p>รายะเอียดการลา</p>
+            <b-form-textarea
+              style="width:340px;height:80px; padding:1px" 
+              v-model.trim="$v.form.description.$model"
+              placeholder="กรอกรายละเอียดการลา"
+              rows="4"
+              no-resize
+              >
+            </b-form-textarea>
+            <div class="error" v-if="!$v.form.description.required"><font color="red">*จำเป็น</font></div>
+            <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+          </div>
+          </div>
+        </div>
+        <div style="margin-top:25px">
+          <b-col sm="12">
+            <center>
+              <vs-button style="margin-top:0px" @click="popupActivo3=true" color="primary" type="filled" to="/leaveUser">
+                บันทึก
+              </vs-button>
+            </center>
+          </b-col>
+        </div>
     </vs-popup>
   </div>
 </template>
@@ -96,7 +113,6 @@
 import * as authService from '@/services/auth.service';
 import datetime from 'vuejs-datetimepicker';
 import { required, minLength } from 'vuelidate/lib/validators'
-
 export default {
   name: "popupLeave",
   components: {
@@ -110,15 +126,7 @@ export default {
             { text: 'ลาครึ่งบ่าย', value: 'second' },
             { text: 'ลาเต็มวัน', value: 'third' }
         ],
-        options1:[
-          {text: '--กรุณาเลือกเหตุผลในการลา--',value: null},
-          {text:'ลากิจ',value:1},
-          {text:'ลาป่วย',value:2},
-          {text:'ลาพักร้อน',value:3},
-          {text:'ลาคลอด',value:4},
-          {text:'ลาบวช',value:5},
-          {text:'ลากิจไม่รับค่าจ้าง',value:6},
-        ],
+        options1:[],
         selected1: null,
         selected2: null,
         value1:'',
@@ -182,14 +190,20 @@ export default {
 };
 </script>
 
-<style scoped>
+<style >
   input[type="date"]::-webkit-inner-spin-button {
       display: none;
       -webkit-appearance: none;
   }
-
-  .popup-example{
+  .popupLeave-example{
     max-width: 100%;
     height: 100%;
+  }
+  #sizePopupLeave .con-vs-popup .vs-popup {
+    width: 35%;
+    height: 63%;
+  }
+  #sizePopupLeave .vs-popup--content {
+    overflow: hidden !important;
   }
 </style>
--- src/components/popupOT.vue
+++ src/components/popupOT.vue
@@ -10,40 +10,39 @@
       </vs-button>
     </div>
 
-      <vs-popup classContent="popup-example"  title="กรอกรายละเอียดการทำงานนอกเวลา" :active.sync="popupOT">
+      <vs-popup id="sizePopupOT" classContent="popupOT-example"  title="กรอกรายละเอียดการทำงานนอกเวลา" :active.sync="popupOT">
         <center><h4>นายเอกชัย หมโมลี</h4></center>
         <center><h5>แผนก : โปรแกรมเมอร์</h5></center>
           <div>
-              <div style="margin:5px 0px 1px 0px" class="form-group" :class="{ 'form-group--error': $v.form.date.$error }">
-                <p>วันที่ทำงาน</p>
+            <div style="margin:5px 0px 1px 0px" class="form-group" :class="{ 'form-group--error': $v.form.date.$error }">
+              <p>วันที่ทำงาน</p>
+              <b-container fluid>
+                <datetime 
+                  v-model.trim="$v.form.date.$model" 
+                  format="DD/MM/YYYY H:i" 
+                  style="width:255px;height:37px;cursor: pointer;"
+                >
+                </datetime>
+                <div class="error" v-if="!$v.form.date.required"><font color="red">*จำเป็น</font></div>
+                <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+              </b-container>
+            </div>
+            <div class="form-group" :class="{ 'form-group--error': $v.form.descriptionWork.$error }">
+              <p>รายละเอียดงาน:</p>
                 <b-container fluid>
-                  <datetime 
-                    v-model.trim="$v.form.date.$model" 
-                    format="DD/MM/YYYY H:i" 
-                    style="width:255px;height:37px;cursor: pointer;"
+                  <b-form-textarea
+                    v-model.trim="$v.form.descriptionWork.$model"
+                    style="width:340px; padding:12px"
+                    id="textarea-no-resize"
+                    placeholder="กรอกรายละเอียดการทำงาน"
+                    rows="4"
+                    no-resize
                   >
-                  </datetime>
-                  <div class="error" v-if="!$v.form.date.required"><font color="red">*จำเป็น</font></div>
+                  </b-form-textarea>
+                  <div class="error" v-if="!$v.form.descriptionWork.required"><font color="red">*จำเป็น</font></div>
                   <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
                 </b-container>
-              </div>
-
-                <div class="form-group" :class="{ 'form-group--error': $v.form.descriptionWork.$error }">
-                  <p>รายละเอียดงาน:</p>
-                    <b-container fluid>
-                      <b-form-textarea
-                        v-model.trim="$v.form.descriptionWork.$model"
-                        style="width:340px; padding:12px"
-                        id="textarea-no-resize"
-                        placeholder="กรอกรายละเอียดการทำงาน"
-                        rows="4"
-                        no-resize
-                      >
-                      </b-form-textarea>
-                      <div class="error" v-if="!$v.form.descriptionWork.required"><font color="red">*จำเป็น</font></div>
-                      <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
-                    </b-container>
-                </div>
+            </div>
 
                 <!-- <div class="form-group" :class="{ 'form-group--error': $v.form.descriptionWork.$error }">
                   <p>รายละเอียดงาน: </p>
@@ -51,51 +50,51 @@
                   <div class="error" v-if="!$v.form.descriptionWork.required"><font color="red">Require</font></div>
                   <div class="error" v-else><font color="Success">Success</font></div>
                 </div>  -->
-                  <div style="margin-top:-13px;">
-                    <b-row class="my-1" v-for="type in types" :key="type">
-                      <b-col>
-                          <div class="form-group" :class="{ 'form-group--error': $v.form.timeStart.$error }">
-                            <p>เวลาเริ่มทำงาน:</p>
-                            <b-container fluid>
-                              <b-form-input :id="`type-${type}`" :type="type" v-model.trim="$v.form.timeStart.$model" style="width:180px;height:37px; cursor: pointer;"> </b-form-input>
-                              <div class="error" v-if="!$v.form.timeStart.required"><font color="red">*จำเป็น</font></div>
-                              <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
-                            </b-container>
-                          </div> 
-                      </b-col>
-                      <b-col>
-                        <div>
-                          <div class="form-group" :class="{ 'form-group--error': $v.form.timeEnd.$error }">
-                            <p>เวลาเลิกงาน:</p>
-                            <b-container fluid>
-                              <b-form-input :id="`type-${type}`" :type="type" v-model.trim="$v.form.timeEnd.$model" style="width:180px;height:37px; cursor: pointer;"> </b-form-input>
-                              <div class="error" v-if="!$v.form.timeEnd.required"><font color="red">*จำเป็น</font></div>
-                              <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
-                          </b-container>
-                          </div> 
-                        </div>
-                      </b-col> 
-                    </b-row>
-                  </div>
+            <div style="margin-top:-13px;">
+              <b-row class="my-1" v-for="type in types" :key="type">
+                <b-col>
+                    <div class="form-group" :class="{ 'form-group--error': $v.form.timeStart.$error }">
+                      <p>เวลาเริ่มทำงาน:</p>
+                      <b-container fluid>
+                        <b-form-input :id="`type-${type}`" :type="type" v-model.trim="$v.form.timeStart.$model" style="width:180px;height:37px; cursor: pointer;"> </b-form-input>
+                        <div class="error" v-if="!$v.form.timeStart.required"><font color="red">*จำเป็น</font></div>
+                        <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+                      </b-container>
+                    </div> 
+                </b-col>
+                <b-col>
                   <div>
-                    <b-form-group>
-                      <b-form-radio-group id="radio-group-2" v-model="selected" name="radio-sub-component">
-                        <center>
-                          <p>เบิกจ่ายเงิน:</p>
-                            <b-form-radio-group
-                              v-model="selected"
-                              :options="options"
-                            ></b-form-radio-group>
-                        </center>
-                      </b-form-radio-group>
-                    </b-form-group>
+                    <div class="form-group" :class="{ 'form-group--error': $v.form.timeEnd.$error }">
+                      <p>เวลาเลิกงาน:</p>
+                      <b-container fluid>
+                        <b-form-input :id="`type-${type}`" :type="type" v-model.trim="$v.form.timeEnd.$model" style="width:180px;height:37px; cursor: pointer;"> </b-form-input>
+                        <div class="error" v-if="!$v.form.timeEnd.required"><font color="red">*จำเป็น</font></div>
+                        <div class="error" v-else><font color="Success"><img src="../assets/Success_icon2.png" width="20" height="20" /></font></div>
+                    </b-container>
+                    </div> 
                   </div>
-                  <div>
-                    <b-col sm="12">
-                      <center>
-                      <vs-button @click="popupActivo3=true" color="primary" type="filled" to="/otworkUser">บันทึก</vs-button></center>
-                    </b-col>
-                  </div>
+                </b-col> 
+              </b-row>
+            </div>
+            <div>
+              <b-form-group>
+                <b-form-radio-group id="radio-group-2" v-model="selected" name="radio-sub-component">
+                  <center>
+                    <p>เบิกจ่ายเงิน:</p>
+                      <b-form-radio-group
+                        v-model="selected"
+                        :options="options"
+                      ></b-form-radio-group>
+                  </center>
+                </b-form-radio-group>
+              </b-form-group>
+            </div>
+            <div>
+              <b-col sm="12">
+                <center>
+                <vs-button @click="popupActivo3=true" color="primary" type="filled" to="/otworkUser">บันทึก</vs-button></center>
+              </b-col>
+            </div>
           </div>
       </vs-popup>     
   </div>
@@ -104,6 +103,7 @@
 <script>
 import datetime from 'vuejs-datetimepicker';
 import { required, minLength } from 'vuelidate/lib/validators'
+
 export default {
   name: "popupOT",
   components: {
@@ -157,5 +157,12 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+#sizePopupOT .con-vs-popup .vs-popup {
+  width: 35%;
+  height: 83%;
+}
+#sizePopupOT .vs-popup--content {
+    overflow: hidden !important;
+  }
 </style>
