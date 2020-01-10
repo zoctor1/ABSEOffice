9758fb9de1034ac238a44895e20909305a36dbb5
inyod <inyod2540ekkachai@gmail.com>
2020-01-10 09:58:35 +0700

    input dataUser

--- src/assets/js/mainJS.js
+++ src/assets/js/mainJS.js
@@ -1,3 +1,10 @@
 export function toPage(page) {
     this.$router.push("/" + page);
 }
+
+export function setDateToServer(d) {
+    var dateTxt = d.split(" ");
+    var setDate = dateTxt[0].split("/");
+    var dateReturn = setDate[2] + "-" + setDate[1] + "-" + setDate[0] + " " + dateTxt[1] + ":00";
+    return dateReturn;
+  }
